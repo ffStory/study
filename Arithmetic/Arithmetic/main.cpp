68c9bb8d98a6ac896435050f096547cdd169ed8e
wangff <wangff@fingerdance.cn>
2020-05-11 14:48:22 +0800

    init

---
+++ Arithmetic/Arithmetic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Arithmetic
+//
+//  Created by FD on 2020/5/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
