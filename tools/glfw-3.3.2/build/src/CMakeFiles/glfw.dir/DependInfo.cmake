27a1662843a3417c259c185be3ce0614b8aa2b8a
wangff <wangff@fingerdance.cn>
2020-05-23 18:52:30 +0800

    OpenGL

---
+++ tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/cocoa_init.m" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/cocoa_joystick.m" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/cocoa_monitor.m" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/cocoa_time.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/cocoa_window.m" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/context.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/egl_context.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/init.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/input.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/monitor.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/nsgl_context.m" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/osmesa_context.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/posix_thread.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/vulkan.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/src/window.c" "/Users/fingerdance/Desktop/study/gitStudy/tools/glfw-3.3.2/build/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
