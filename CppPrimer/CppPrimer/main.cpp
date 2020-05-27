530def693ddddedc4eaa680b41b62f4a9722212c
wangff <wangff@fingerdance.cn>
2020-05-27 20:13:50 +0800

    add CppPrimer

---
+++ CppPrimer/CppPrimer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppPrimer
+//
+//  Created by FD on 2020/5/27.
+//  Copyright © 2020 FD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
